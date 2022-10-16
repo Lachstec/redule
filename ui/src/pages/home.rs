1f270abfba17a652ad8fd6e304c68b7da597b96e
leonlux <leonlux@icloud.com>
2022-10-16 17:45:24 +0200

    added hover effect to frontpage buttons

--- ui/src/pages/home.rs
+++ ui/src/pages/home.rs
@@ -28,8 +28,8 @@ pub fn home() -> Html {
                         {"the easy way."}
                         <div class=" mt-1 text-xl text-gray-400">{"Quickly find the perfect Day and Time"}</div>
                         <div class="space-x-3">
-                            <Link<Route> classes={classes!("rounded", "text-lg", "bg-sky-500", "px-5", "py-3", "font-semibold", "text-white", "shadow-sm")} to={Route::NewPoll}> {"Start Planning"}</Link<Route>>
-                            <Link<Route> classes={classes!("rounded", "text-lg", "bg-gray-500", "px-5", "py-3", "font-semibold", "text-white", "shadow-sm")} to={Route::Help}> {"Getting Started"}</Link<Route>>
+                            <Link<Route> classes={classes!("rounded", "hover:bg-sky-700", "transition", "text-lg", "bg-sky-500", "px-5", "py-3", "font-semibold", "text-white", "shadow-sm")} to={Route::NewPoll}> {"Start Planning"}</Link<Route>>
+                            <Link<Route> classes={classes!("rounded", "hover:bg-gray-700", "transition", "text-lg", "bg-gray-500", "px-5", "py-3", "font-semibold", "text-white", "shadow-sm")} to={Route::Help}> {"Getting Started"}</Link<Route>>
                         </div>
                     </h1>                    
                 </div>
